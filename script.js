13874020b10494a88df1a48b5ea879e8d8c0d5f7
Dandush03 <d.laloush@outlook.com>
2020-01-29 19:00:24 -0400

    Added New Funciton JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
+window.onscroll = function() {scrolling()};
+window.onresize = function() {this.resizes()}
+function scrolling() {
   if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
     document.getElementById("tp-nv").style.display = "none";
     document.getElementById("lg-dv").style.height = "40px";
@@ -10,5 +10,15 @@ function myFunction() {
       document.getElementById("tp-nv").style.display = "unset";
     }
     document.getElementById("lg-dv").style.height = "70px";
+  }
+}
+
+function resizes() {
+  if(window.innerWidth < 768 && document.body.scrollTop > 70 ){
+    document.getElementById("tp-nv").style.display = "none";
+  }else{
+    if (document.body.scrollTop < 70 ) {
+      document.getElementById("tp-nv").style.display = "unset";
+    }
   }
 }
